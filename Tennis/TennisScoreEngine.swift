bab354a748bd23f1d7c8a6ae48ff8b00eeee36ec
2019-DEV-184 <2019.DEV.184@gmail.com>
2019-07-17 17:18:14 +0530

    Player1 points levels added

--- Tennis/TennisScoreEngine.swift
+++ Tennis/TennisScoreEngine.swift
@@ -10,11 +10,30 @@ import Foundation
 
 final class TennisScoreEngine {
     
+    enum Points: Int {
+        case love = 0
+        case fifteen = 1
+        case thirty = 2
+        case forty = 3
+    }
+    
     let player1Name: String
     let player2Name: String
+    
+    private(set) var player1Score: Points = .love
+    private(set) var player2Score: Points = .love
     
     init(_ player1Name: String, _ player2Name: String) {
         self.player1Name = player1Name
         self.player2Name = player2Name
+    }
+    
+    func player1Scores() {
+        switch player1Score {
+        case .love: player1Score = .fifteen
+        case .fifteen: player1Score = .thirty
+        case .thirty: player1Score = .forty
+        case .forty: player1Score = .love
+        }
     }
 }
--- TennisTests/TennisTests.swift
+++ TennisTests/TennisTests.swift
@@ -22,7 +22,49 @@ class TennisTests: XCTestCase {
         // then
         XCTAssertEqual(sut.player1Name, "Player1")
         XCTAssertEqual(sut.player2Name, "Player2")
+        XCTAssertEqual(sut.player1Score, .love)
+        XCTAssertEqual(sut.player2Score, .love)
     }
     
+    func test_Player1ScoresOnce_Player1ScoreIs15() {
+        // given
+        let player1 = "Player1"
+        let player2 = "Player2"
+        let sut = TennisScoreEngine(player1, player2)
+        
+        // when
+        sut.player1Scores()
+        
+        // then
+        XCTAssertEqual(sut.player1Score, .fifteen)
+    }
     
+    func test_Player1ScoresTwice_Player1ScoreIs30() {
+        // given
+        let player1 = "Player1"
+        let player2 = "Player2"
+        let sut = TennisScoreEngine(player1, player2)
+        
+        // when
+        sut.player1Scores()
+        sut.player1Scores()
+        
+        // then
+        XCTAssertEqual(sut.player1Score, .thirty)
+    }
+    
+    func test_Player1ScoresThrice_Player1ScoreIs40() {
+        // given
+        let player1 = "Player1"
+        let player2 = "Player2"
+        let sut = TennisScoreEngine(player1, player2)
+        
+        // when
+        sut.player1Scores()
+        sut.player1Scores()
+        sut.player1Scores()
+        
+        // then
+        XCTAssertEqual(sut.player1Score, .forty)
+    }
 }
